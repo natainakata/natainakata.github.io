05f4bef388eb6119dcb9985cd0252841afe1908b
natai nakata <satzin0521@gmail.com>
2024-11-07 12:50:41 +0900

    style: add M PLUS 1p

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,9 @@ const config: Config = {
         "7xl": "4.5rem",
         "8xl": "6.25rem",
       },
+      fontFamily: {
+        sans: ["M PLUS 1p", "sans-serif"],
+      },
       boxShadow: {
         sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
         md: "0 8px 30px rgba(0, 0, 0, 0.12)",
