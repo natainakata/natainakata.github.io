7161b0d90a3307a7a2b9275ff6f236faa906272e
natai nakata <satzin0521@gmail.com>
2024-11-13 22:02:36 +0900

    style: シンタックスハイライトをrehype-pretty-codeで設定

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,10 +1,5 @@
-"use client";
-import React, { useEffect } from "react";
-import Prism from "prismjs";
 import "prism-themes/themes/prism-one-dark.min.css";
-import "prismjs/components/prism-bash";
-import "prismjs/components/prism-typescript";
-import "prismjs/components/prism-jsx";
+
 import "../styles/markdown.css";
 
 type Props = {
@@ -12,13 +7,10 @@ type Props = {
 };
 
 export function PostBody({ content }: Props) {
-  useEffect(() => {
-    Prism.highlightAll();
-  });
   return (
     <div className="max-w-4xl mx-auto">
       <div
-        className="prose md:prose-2xl"
+        className="prose lg:prose-xl md:prose-lg mx-auto"
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
